92e27f1d6c4c4d442f9b211e269eb943149ece1f
CaQtiml <37920301+CaQtiml@users.noreply.github.com>
2020-05-16 21:40:55 +0700

    Add files via upload

---
+++ countinversion_mergesort.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+long long ans = 0;
+void mergei(int a[], int st, int ed) {
+  int mid = (st+ed)>>1;
+  int i = st;
+  int s = st;
+  int *arr = new int[ed - st + 1];
+  int j = mid+1;
+  int k = 0;
+  while (i <= mid && j <= ed) {
+    if (a[i] <= a[j]) {
+      arr[k++] = a[i++];
+      
+    } else {
+      arr[k++] = a[j++];
+      ans += ((mid + 1) - i);
+    }
+  }
+  for (; i <= mid;)
+    arr[k++] = a[i++];
+  for (; j <= ed;)
+    arr[k++] = a[j++];
+  for (k = 0; s <= ed;)
+    a[s++] = arr[k++];
+  delete[] arr;
+}
+
+void m_sort(int a[], int st, int ed) {
+  if (st < ed) {
+    int mid  = (st+ed)>>1;
+    m_sort(a, st, mid);
+    m_sort(a, mid + 1, ed);
+    mergei(a, st, ed);
+  }
+}
+int main() {
+  int t;
+  scanf("%d", &t);
+  while (t--) {
+    int n;
+    ans = 0;
+    scanf("%d", &n);
+    int *a = new int[n];
+    for (int i = 0; i < n; i++)
+      scanf("%d", &a[i]);
+    m_sort(a, 0, n - 1);
+    cout << ans << endl;
+  }
+  return 0;
+}
