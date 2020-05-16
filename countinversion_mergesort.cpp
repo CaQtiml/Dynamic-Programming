ea100231ae96b484e86ae7af8f62b1a47f66c1fd
CaQtiml <37920301+CaQtiml@users.noreply.github.com>
2020-05-16 21:54:22 +0700

    Add files via upload

--- countinversion_mergesort.cpp
+++ countinversion_mergesort.cpp
@@ -5,16 +5,16 @@ using namespace std;
 
 long long ans = 0;
 void mergei(int a[], int st, int ed) {
-  int mid = (st+ed)>>1;
+  int mid = (st + ed) >> 1;
   int i = st;
-  int s = st;
   int *arr = new int[ed - st + 1];
-  int j = mid+1;
+  //int s = st;
+  int j = mid + 1;
   int k = 0;
   while (i <= mid && j <= ed) {
     if (a[i] <= a[j]) {
       arr[k++] = a[i++];
-      
+
     } else {
       arr[k++] = a[j++];
       ans += ((mid + 1) - i);
@@ -24,14 +24,14 @@ void mergei(int a[], int st, int ed) {
     arr[k++] = a[i++];
   for (; j <= ed;)
     arr[k++] = a[j++];
-  for (k = 0; s <= ed;)
+  for (int k = 0, s = st; s <= ed;)
     a[s++] = arr[k++];
   delete[] arr;
 }
 
 void m_sort(int a[], int st, int ed) {
   if (st < ed) {
-    int mid  = (st+ed)>>1;
+    int mid = (st + ed) >> 1;
     m_sort(a, st, mid);
     m_sort(a, mid + 1, ed);
     mergei(a, st, ed);
